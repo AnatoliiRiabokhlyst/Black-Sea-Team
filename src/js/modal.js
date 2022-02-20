336e32b8f16b33f03dbc5bd503de85066ad98cc7
Oleh Honchar <ooollleeeg@gmail.com>
2022-02-20 18:53:42 +0200

    добавляет javascript на закрытие и открытие модального окна

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,1 +1,15 @@
-console.log("hello from modal");
+// console.log("hello from modal");
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
