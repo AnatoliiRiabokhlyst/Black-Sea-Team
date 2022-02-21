08c2bda24a8254c42b5a48df8d78efac3ceaca9e
Vladymyr Shadura <vs131083@gmail.com>
2022-02-21 19:17:59 +0200

    add

--- src/js/about.js
+++ src/js/about.js
@@ -1,7 +1,7 @@
 // (() => {
     const aboutBtn=document.querySelector('[data-about-open]')
     const aboutText = document.querySelector('[data-about-hidden]')
-    console.log('hello');
+    // console.log('hello');
     aboutBtn.addEventListener('click', () => {
         aboutText.classList.toggle('is-hidden')
     })
